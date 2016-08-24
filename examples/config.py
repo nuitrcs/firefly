b1f919fc19a65366ae4d2a0653777d17c63edc76
Alessandro Febretti <febret@gmail.com>
2016-08-24 14:58:55 -0500

    improving example config

--- examples/config.py
+++ examples/config.py
@@ -5,13 +5,7 @@ for i in range(0, 4):
     files.append(datasetBase + 'snapshot_440.' + str(i) + '.hdf5')
 datasetPath = ';'.join(files)
 
-# use this to load a single file.
-# <<<<<<< Updated upstream
-datasetPath = 'snapshot_140.hdf5'
-# =======
-#datasetPath = 'C:/Users/defaultuser0/snapshot_140.hdf5'
-# >>>>>>> Stashed changes
-
+datasetPath = os.path.abspath('snapshot_600.hdf5')
 
 # Initial pivot.
 pivotPosition = Vector3(47, 17, 62)
@@ -19,3 +13,15 @@ pivotPosition = Vector3(47, 17, 62)
 # Scale to apply to the entire 3D dataset. Useful if the dataset is too large,
 # to simplify navigation
 scale = 0.01
+
+# change the last number to choose a different decimation level for data loading
+pointCloudLoadOptions = "50000 0:100000:1"
+
+# This sets the dynamic quality rendering mode. Higher number = faster rendering but
+# lower quality
+dqDec = 4
+
+# This is the name of the script we want to use to load the file. Different simulation
+# snapshots might be outputting different DataParts / columns. You can have a different
+# loader script for each simulation output config.
+loaderScript = 'firefly/loader.py'
